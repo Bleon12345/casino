4b11ff1f91c41db720133a05d7851838d488338a
Bleon12345 <72931239+Bleon12345@users.noreply.github.com>
2024-12-18 23:56:45 +0100

    the weather outside is rizzy

---
+++ rizzy.py
@@ -0,0 +1,12 @@
+song = "The weather oustide is rizzy but the fire is so skibidii but since i've GYATT to go Ohio Ohio Ohio!"
+
+still_water = []
+words = song.split()
+
+for word in words:
+    for i in range(len(word)- 1):
+        if word[i] == word[i+1]:
+            still_water.append(word)
+            break
+
+print(still_water)
